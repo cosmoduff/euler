d6643615a737e8d73d55e05143556ca5fc0b913b
Seth SWanson <want2work@gmail.com>
2015-10-18 20:04:01 -0400

    Added package, file and service resources to init.pp. Added dnsmasq.conf from arch linux to the files directory

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,15 @@
 class dnsmasq {
-        package {dnsmasq
+        package { 'dnsmasq':
+            ensure => installed,
+        }
+        
+        file { '/etc/dnsmasq.conf':
+            ensure  => present,
+            require => Package['dnsmasq'],
+        }
+
+        service { 'dnsmsq':
+            ensure    => running,
+            enabled   => true,
+            subscribe => File['/etc/dnsmasq.conf'],
+        }
