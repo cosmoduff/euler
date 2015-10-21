73b689771ba21470e855e72d4b80c54a9e489239
Seth SWanson <want2work@gmail.com>
2015-10-21 11:23:26 -0400

    fixed class parameters by adding ''

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,8 @@
 class dnsmasq::params {
 
-    $default_ip = 1.2.3.4
-    $domains    = [bob.com,
-                 alice.com,]
+    $default_ip = '1.2.3.4'
+    $domains    = ['bob.com',
+                 'alice.com',]
 
-    $defult_sec_dns = 8.8.8.8
+    $defult_sec_dns = '8.8.8.8'
 }
