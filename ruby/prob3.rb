bf2a775a54069db632fdb07d0b8c17acddef41b3
Seth SWanson <want2work@gmail.com>
2015-11-02 15:34:04 -0500

    Worked on primary function to get solution

--- ruby/prob3.rb
+++ ruby/prob3.rb
@@ -1,6 +1,16 @@
+#Work in progress`
 #What is the largest prime factor of the number 600851475143 ?
 #
 #
 def prime_factor(n)
-    i = 2
-    
+   for i in 2..n
+       if n % i == 0
+           i = i / n
+           puts #(i)
+       end
+   end
+end
+
+num = 600851475143
+
+prime_factor(num)
