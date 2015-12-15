4ea7ad65054d1587b469a4495c8cf98e78c2ac2f
griff <sethswanson@gmail.com>
2015-12-15 11:22:07 -0500

    Changed findTriangle func to genTriangle. Now it finds the next nth triangle. Modified the loop accordingly.

--- python/prob12.py
+++ python/prob12.py
@@ -4,31 +4,28 @@
 
 import math
 
-def findTriangle(x):
-    triangle = 0
-    for y in range(1,x+1):
-        triangle += y
+def genTriangle(x):
+    triangle = (x*(x+1))/2
     return triangle
 
 def countDivisors(x):
     count = 0
-    for y in range(1,math.sqrt(x)):
+    for y in range(1,int(math.sqrt(x))):
         if x % y == 0:
             count += 1
     return count
 
-number = input("Enter a number: ")
+#number = input("Enter a number: ")
 
-#number = 1000
+number = 1
 
-"""while True:
-    count = countDivisors(findTriangle(int(number)))
+while True:
+    trianlge = genTriangle(number)
+    count = countDivisors(int(number))
     print(number, count)
     if count > 500:
         break
     else:
-        number += 1000
-"""
-
-print(findTriangle(int(number)))
-print(countDivisors(int(number)))
+        number += 1
+# notes
+# add prime check to quickly move on as a prime only has two divisors
