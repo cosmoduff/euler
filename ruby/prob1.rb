398a8cc57704fe67ef9a7205d94306cb833bc28e
Seth Swanson <sethswanson@gmail.com>
2016-01-11 17:37:55 -0500

    Changed problem 1 to be more ruby idiomatic

--- ruby/prob1.rb
+++ ruby/prob1.rb
@@ -1,13 +1,11 @@
 #Find the sum of all the multiples of 3 or 5 below 1000.
 
-i = 3
 sum = 0
 
-while i < 1000
-    if i % 3 == 0 || i % 5 == 0 then
+for i in 3...1000
+    if i % 3 == 0 || i % 5 == 0
         sum = sum + i
     end
-    i = i + 1
 end
 
 puts sum
