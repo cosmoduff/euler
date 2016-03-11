8d69e0dbeaaeb2eb40f84983f41b93088d0e3d15
Seth Swanson <sethswanson@gmail.com>
2016-03-10 19:34:03 -0500

    Completed problem 2

--- rust/prob2.rs
+++ rust/prob2.rs
@@ -1,2 +1,18 @@
 // By considering the terms in the Fibonacci sequence whose values do not exceed four million find the sum of the even-valued terms
 
+fn main() {
+        let mut first_n = 1;
+        let mut second_n = 1;
+        let mut fib = 0;
+        let mut total = 0;
+
+        while fib < 4000000 {
+                fib = first_n + second_n;
+                if fib % 2 == 0 {
+                        total += fib;
+                }
+                first_n = second_n;
+                second_n = fib;
+        }
+        println!("{}", total);
+}
