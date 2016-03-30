49473f0387337a44bd88cedeed4d80fe9da9538b
Swanson <seth.swanson@wpz-266630.howardu.net>
2016-03-30 12:25:19 -0400

    Changed value to the example given in the problem Added primality check function but did not implement

--- python/prob12.py
+++ python/prob12.py
@@ -4,6 +4,20 @@
 
 import math
 
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i == 5
+    while i * i <=n:
+        if n % i == 0 or n % (i + 2) == 0:
+                return False
+        i = i + 6
+    return True
+
 def genTriangle(x):
     triangle = (x*(x+1))/2
     return triangle
@@ -20,10 +34,11 @@ def countDivisors(x):
 number = 1
 
 while True:
-    trianlge = genTriangle(number)
-    count = countDivisors(int(number))
-    print(number, count)
-    if count > 500:
+    triangle = genTriangle(number)
+    count = countDivisors(int(triangle))
+    # print(number, count)
+    if count > 5:
+        print(triangle)
         break
     else:
         number += 1
