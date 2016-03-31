66829d6e1ba743501107e1967dac4a31b1706576
Seth Swanson <sethswanson@gmail.com>
2016-03-31 19:39:30 -0400

    Solved problem 3 in rust Implementation needs work

--- rust/prob3/src/main.rs
+++ rust/prob3/src/main.rs
@@ -1,7 +1,16 @@
 //What is the largest prime factor of the number 600851475143?
 
-let num = 600851475143;
+fn prime_factor(n: u64) {
+    let mut num = n;
+    for x in 2..num {
+        if num % x == 0 {
+            num = num / x;
+            println!("{}", x);
+        }
+    }
+}
 
-fn isPrime() {
-
+fn main() {
+    let num = 600851475143;
+    prime_factor(num);
 }
