f63ea620086b010b8509d644485e4ce934c32f0d
Swanson <seth.swanson@wpz-266630.howardu.net>
2016-04-14 13:33:32 -0400

    Cleaned up a bit

--- python/prob10.py
+++ python/prob10.py
@@ -45,15 +45,16 @@ thread1 = SummingPrimes(1, 750000)
 thread2 = SummingPrimes(750000, 1500000)
 thread3 = SummingPrimes(1500000, 1750000)
 thread4 = SummingPrimes(1750000, 2000000)
+
 thread1.start()
 thread2.start()
 thread3.start()
 thread4.start()
+
 thread1.join()
 thread2.join()
 thread3.join()
 thread4.join()
 
 result = thread1.total + thread2.total + thread3.total + thread4.total
-
 print(result)
