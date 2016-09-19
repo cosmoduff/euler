0d685a64744c5a94ffb6afac33925f21b1b772a0
Seth Swanson <sethswanson@gmail.com>
2016-09-19 10:32:22 -0400

    Completed remainder function

--- rust/prob5/src/main.rs
+++ rust/prob5/src/main.rs
@@ -1,16 +1,17 @@
-// What is the smallest positive number that is evenly divisible by all the numbers from 1 to 20
+// What is the smallest positive number that is evenly divisible by all the 
+// numbers from 1 to 20
 
-fn remainder (n: u32) -> bool {
-    for x in 20..2 {
-	if n%x == 0 {
-	    continue;
-	} else {
-	    return False
-	}
+fn remainder(x: u32) -> bool {
+    for y in 20..2 {
+        if x % y == 0 {
+            continue;
+        } else {
+            return false;
+        }
     }
 }
 
 fn main() {
+    base_num = 2520;
 
-    println!("Hello, world!");
 }
