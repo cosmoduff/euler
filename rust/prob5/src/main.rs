35c8f81fed04f82ca42594220ba0075cd394847a
Seth Swanson <sethswanson@gmail.com>
2016-09-20 10:18:55 -0400

    Completed rust problem 5

--- rust/prob5/src/main.rs
+++ rust/prob5/src/main.rs
@@ -1,17 +1,24 @@
 // What is the smallest positive number that is evenly divisible by all the 
 // numbers from 1 to 20
 
-fn remainder(x: u32) -> bool {
-    for y in 20..2 {
+fn remainder(x: u64) -> bool {
+    let mut escape = true;
+    for y in 2..21 {
         if x % y == 0 {
             continue;
         } else {
-            return false;
+            escape = false;
         }
     }
+    return escape;
 }
 
 fn main() {
-    base_num = 2520;
+    let mut base_num = 2520;
 
+    while !remainder(base_num) {
+        base_num += 2520
+    }
+    
+    println!("{}", base_num);
 }
