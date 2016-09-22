8ab90afa73b48acd58c3cf3965f99dc1b5753029
Seth Swanson <sethswanson@gmail.com>
2016-09-22 11:50:40 -0400

    Solved rust problem 7

--- rust/prob7/src/main.rs
+++ rust/prob7/src/main.rs
@@ -3,16 +3,16 @@
 fn is_prime(x: u64) -> bool {
    if x <= 1 {
        return false;
-   } else if n <= 3 {
+   } else if x <= 3 {
        return true;
-   } else if n % 2 == 0 || n % 3 == 0 {
+   } else if x % 2 == 0 || x % 3 == 0 {
        return false;
    }
 
-   let mut i = 5;
+   let mut i: u64 = 5;
 
    while i * i <= x {
-       if x % i == 0 or n % (i + 2) == 0 {
+       if x % i == 0 || x % (i + 2) == 0 {
             return false;
        }
        
@@ -22,15 +22,15 @@ fn is_prime(x: u64) -> bool {
 }
 
 fn main() {
-    let mut u64: n = 0;
-    let mut u64: number = 1;
+    let mut n: u64 = 0;
+    let mut number: u64 = 0;
 
     while n < 10001 {
-        if is_prime(number) {
-            i = i + 1;
-        }
-
         number = number + 1;
+
+        if is_prime(number) {
+            n = n + 1;
+        }
     }
 
     println!("{}", number);
