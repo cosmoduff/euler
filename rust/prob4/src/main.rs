46547beb3aab9e59ba8798148aa83faf6fc96f3b
Seth Swanson <sethswanson@gmail.com>
2016-09-22 11:27:42 -0400

    Completed rust prob4

--- rust/prob4/src/main.rs
+++ rust/prob4/src/main.rs
@@ -1,6 +1,29 @@
-// Find the largest palindrome amde from the product of two
+// Find the largest palindrome made from the product of two
 // 3-digit numbers
 
+fn is_palindrome(s: String) -> bool {
+    let reverse: String = s.chars().rev().collect();
+    
+    if s == reverse {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut palindrome: u32 = 0;
+    let mut product: u32;
+    for x in (100..1000).rev() {
+        for y in (100..1000).rev() {
+            product = x * y;
+            if is_palindrome(product.to_string()) {
+                if product > palindrome {
+                    palindrome = product;
+                    break
+                }
+            }
+        }
+    }
+    println!("{}", palindrome);
 }
