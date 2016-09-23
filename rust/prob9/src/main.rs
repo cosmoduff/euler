663de0a04322c3d7d57f98c95c4ab479438a0f3f
Seth Swanson <sethswanson@gmail.com>
2016-09-23 19:43:51 -0400

    Fixed rust problem 9

--- rust/prob9/src/main.rs
+++ rust/prob9/src/main.rs
@@ -1,15 +1,19 @@
 // There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 // Find the product abc
 
-fn main() {
-    'a: for a in (1..501).rev() {
-        'b: for b in (1..501).rev() {
-            'c: for c in (1..501).rev() {
-                if a + b + c == 1000 && (a*a) + (b*b) == (c*c) {
+fn pythagorean_triplet(n: u32) {
+    for a in (1..501).rev() {
+        for b in (1..501).rev() {
+            for c in (1..501).rev() {
+                if a + b + c == n && (a*a) + (b*b) == (c*c) {
                     let product = a * b * c;
                     println!("{}", product);
                 }
             }
         }
     }
+}
+
+fn main() {
+    pythagorean_triplet(1000)
 }
