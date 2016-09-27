be833d6f942bb6afb0c0a87a7a80c3e54ab6f6d3
Seth Swanson <sethswanson@gmail.com>
2016-09-27 10:16:36 -0400

    Created create_traingle_num function for rust prob 12

--- rust/prob12/src/main.rs
+++ rust/prob12/src/main.rs
@@ -1,6 +1,17 @@
 // What is the value of the first triangle number to have over five hundred
 // divisors
 
+fn create_triangle_num(x: u64) -> u64 {
+    let end_range = x + 1;
+    let mut product: u64 = 0;
+
+    for y in 1..end_range {
+        product = product + y
+    }
+    return product;
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Test triangle function. Should print 28
+    println!("{}", create_triangle_num(7));
 }
