3a459eec5fa9e3a96d4da6fe910e0a97bbfc2d10
Seth Swanson <sethswanson@gmail.com>
2016-11-17 14:46:40 -0500

    Completed python problem 15

--- python/prob15.py
+++ python/prob15.py
@@ -2,14 +2,4 @@
 
 import euler
 
-def lattice_patterns(n):
-    points = n + 1
-    first_row=[]
-    second_row=[]
-    for i in range(0,points):
-        first_row.append(1)
-    print(first_row)
-
-
-print(euler.factorial(5))
-lattice_patterns(2)
+print(euler.lattice_routes(40, 20))
