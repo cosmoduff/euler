250c07af65fe2a5b05f46c620562a03ae9201fa5
Seth Swanson <sethswanson@gmail.com>
2017-03-17 18:47:19 -0400

    Greatly simplified and improved python prob16

--- python/prob16.py
+++ python/prob16.py
@@ -7,11 +7,7 @@ num = 2**1000
 def add_digits(n):
 # Takes a number n and returns the sum of the digits of that number
 # Ex 32768 3+2+7+6+8=26
-    sum = 0
-
-    for x in str(n):
-        sum += int(x)
-
-    return sum
+    result = sum(list(map(int, str(n))))
+    return result 
 
 print(add_digits(num))
