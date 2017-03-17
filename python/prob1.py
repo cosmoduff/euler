6bdfc07f2f9accaee9888b13844e83915791725b
Seth Swanson <sethswanson@gmail.com>
2017-03-17 19:13:18 -0400

    Improved python prob1 using a list

--- python/prob1.py
+++ python/prob1.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 #Find the sum of all the multiples of 3 or 5 below 1000
 
-total = 0
+divis = []
 
 for n in range(1,1000,1):
     if (n%3==0) or (n%5==0):
-        total = total + n
-        print(total)
+        divis.append(n)
 
-print(total)
+print(sum(divis))
